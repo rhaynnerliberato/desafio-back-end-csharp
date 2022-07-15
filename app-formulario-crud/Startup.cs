43f81f781ecc4c3601055548a3c6df44d2bd2757
rhaynner-liberato <rhaynner.liberato@autoglass.com.br>
2022-07-15 18:10:17 -0300

    Foi implementado class do bootstrap em algus botoes

--- app-formulario-crud/Startup.cs
+++ app-formulario-crud/Startup.cs
@@ -1,14 +1,9 @@
 using app_formulario_crud.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace app_formulario_crud
 {

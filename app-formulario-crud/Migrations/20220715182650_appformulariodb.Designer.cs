c022374370f3f74dc03e4a6d9e71d4077aad42b7
rhaynner-liberato <rhaynner.liberato@autoglass.com.br>
2022-07-15 16:22:06 -0300

    foram removidas importacoes desnecessarias

--- app-formulario-crud/Migrations/20220715182650_appformulariodb.Designer.cs
+++ app-formulario-crud/Migrations/20220715182650_appformulariodb.Designer.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using app_formulario_crud.Models;
 
 namespace app_formulario_crud.Migrations

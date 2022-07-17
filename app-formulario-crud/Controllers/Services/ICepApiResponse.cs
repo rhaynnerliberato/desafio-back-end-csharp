44b79e8524b1fb7c4939debafac5b0469ffb60e0
rhaynner-liberato <rhaynner.liberato@autoglass.com.br>
2022-07-17 20:24:48 -0300

    aplicacao de bootstrap e validacao de alguns campos do formulario

---
+++ app-formulario-crud/Controllers/Services/ICepApiResponse.cs
@@ -0,0 +1,12 @@
+﻿using app_formulario_crud.Models;
+using Refit;
+using System.Threading.Tasks;
+
+namespace app_formulario_crud.Controllers.Services
+{
+    public interface ICepApiResponse
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAddressAsync(string cep);
+    }
+}
